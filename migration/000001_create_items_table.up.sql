03c869adf0aa2e0be23e99a5e877fdefd6374114
Acidspud <acidspud@123.com>
2024-12-12 00:43:41 +0200

    Bump the item id's for the migration data

--- migration/000001_create_items_table.up.sql
+++ migration/000001_create_items_table.up.sql
@@ -10,16 +10,16 @@ CREATE TABLE IF NOT EXISTS items(
 
 INSERT INTO items
 (id, "name", qty, threshold, price, created_at, updated_at)
-VALUES(1, 'ItemA', 11, 9, 2099, '2023-07-10 13:01:07.264', '2023-07-10 13:36:16.891');
+VALUES(11, 'ItemA', 11, 9, 2099, '2023-07-10 13:01:07.264', '2023-07-10 13:36:16.891');
 INSERT INTO items
 (id, "name", qty, threshold, price, created_at, updated_at)
-VALUES(4, 'ItemD', 10, 9, 22200, '2023-07-09 19:20:18.213', '2023-07-10 13:37:03.356');
+VALUES(14, 'ItemD', 10, 9, 22200, '2023-07-09 19:20:18.213', '2023-07-10 13:37:03.356');
 INSERT INTO items
 (id, "name", qty, threshold, price, created_at, updated_at)
-VALUES(5, 'ItemE', 10, 9, 2250, '2023-07-10 13:01:53.827', '2023-07-10 13:37:46.457');
+VALUES(15, 'ItemE', 10, 9, 2250, '2023-07-10 13:01:53.827', '2023-07-10 13:37:46.457');
 INSERT INTO items
 (id, "name", qty, threshold, price, created_at, updated_at)
-VALUES(2, 'ItemB', 7, 9, 2099, '2023-07-10 13:02:48.946', '2023-07-10 13:38:40.871');
+VALUES(12, 'ItemB', 7, 9, 2099, '2023-07-10 13:02:48.946', '2023-07-10 13:38:40.871');
 INSERT INTO items
 (id, "name", qty, threshold, price, created_at, updated_at)
-VALUES(3, 'ItemC', 9, 9, 30000000, '2023-07-09 19:19:31.292', '2023-07-10 13:41:02.873');
+VALUES(13, 'ItemC', 9, 9, 30000000, '2023-07-09 19:19:31.292', '2023-07-10 13:41:02.873');
